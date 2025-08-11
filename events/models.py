6b2e4298b6354d761ff781eaa1a61f090789c2a0
Med Amine <ouldkhaoua.pro@gmail.com>
2025-08-11 15:10:30 +0100

    Update models.py

--- events/models.py
+++ events/models.py
@@ -79,14 +79,18 @@ class Event(models.Model):
             pass
 
         super().save(*args, **kwargs)
-        
-        # Resize image
-        if Image and self.image and os.path.isfile(self.image.path):
-            img = Image.open(self.image.path)
-            if img.height > 800 or img.width > 800:
-                output_size = (800, 800)
-                img.thumbnail(output_size)
-                img.save(self.image.path)
+
+        # Resize image only when stored on local filesystem (e.g., dev or non-cloud storage)
+        image_path = getattr(self.image, 'path', None)
+        if Image and self.image and image_path and os.path.isfile(image_path):
+            try:
+                img = Image.open(image_path)
+                if img.height > 800 or img.width > 800:
+                    output_size = (800, 800)
+                    img.thumbnail(output_size)
+                    img.save(image_path)
+            except Exception:
+                pass
 
 
 class RSVP(models.Model):
