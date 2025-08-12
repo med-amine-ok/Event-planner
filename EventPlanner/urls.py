b32ecec174f7d7cd729ebbc008544ee460594c6e
Med Amine <ouldkhaoua.pro@gmail.com>
2025-08-12 18:26:39 +0100

    Update urls.py

--- EventPlanner/urls.py
+++ EventPlanner/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Serve media files in both development and production
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
