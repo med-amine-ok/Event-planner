7ded78510000c5e27613a2d2cc5eafd4ceadc349
Med Amine <ouldkhaoua.pro@gmail.com>
2025-08-12 18:40:06 +0100

    Update urls.py

--- EventPlanner/urls.py
+++ EventPlanner/urls.py
@@ -18,11 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import MediaServeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('events.urls')),
     path('accounts/', include('accounts.urls')),
+    path('media/<path:path>', MediaServeView.as_view(), name='media'),
 ]
 
 # Serve media files in both development and production
